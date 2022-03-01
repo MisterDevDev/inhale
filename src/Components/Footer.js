7e6905e291b77d46e08621deaa9e9bae46d29c6b
Devin Boyd <djb91c@gmail.com>
2022-03-01 12:31:17 -0500

    fixed footer button, bold close button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         style={{ backgroundColor: "#ff8c00" }}
         variant="contained"
         size="large"
-        onClick={() => navigate.push("/students")}
+        onClick={() => location.href= "https://www.amazon.com/Inhale-Respiratory-Trainer-Breathing-Exercise/dp/B091L441KW/ref=sr_1_5?crid=36QXB5CVFJQ2L&keywords=inhale+breather&qid=1646154061&sprefix=inhale+breather%2Caps%2C87&sr=8-5"}
       >
         Buy Now
       </Button>
